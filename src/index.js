4c6bcd76a99b859fa647556abee9125abc913742
Nikolay <77922872+nBonev@users.noreply.github.com>
2025-01-21 15:51:14 +0200

    Add routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
-import homeController from './controllers/homeController.js';
+import routes from './routes.js';
+
 
 const app = express();
 
@@ -12,11 +13,9 @@ app.set('views', './src/views');
 
 app.use('/static', express.static('src/public'));
 
-app.use(homeController);
+app.use(routes);
 
-app.get('*', (req, res) => {
-    res.render('404');
-});
+
 
 
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
