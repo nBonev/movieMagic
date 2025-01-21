555b99a6a8c8bcd9463714ef8710efd5513a6775
Nikolay <77922872+nBonev@users.noreply.github.com>
2025-01-21 15:40:51 +0200

    Add home cotroller

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
+import homeController from './controllers/homeController.js';
 
 const app = express();
 
@@ -11,13 +12,7 @@ app.set('views', './src/views');
 
 app.use('/static', express.static('src/public'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
-app.get('/about', (req, res) => {
-    res.render('about');
-});
+app.use(homeController);
 
 app.get('*', (req, res) => {
     res.render('404');
