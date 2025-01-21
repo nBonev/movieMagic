7e82adeea61824a2b4f2f6fb41a5d5179a202180
Nikolay <77922872+nBonev@users.noreply.github.com>
2025-01-21 16:44:16 +0200

    Add dynamic details link for each movie

--- src/movies.js
+++ src/movies.js
@@ -1,17 +1,20 @@
 export default [
     {
+        id: 1,
         title: 'Jungle Cruise',
         genre: 'Adventure',
         description: 'Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.',
         imageUrl: '/static/img/jungle-cruise.jpeg'
     },
     {
+        id: 2,
         title: 'Man of Steel',
         genre: 'Superhero',
         description: 'An alien child is evacuated from his dying world and sent to Earth to live among humans. His peace is threatened when other survivors of his home planet invade Earth.',
         imageUrl: 'https://m.media-amazon.com/images/M/MV5BMTk5ODk1NDkxMF5BMl5BanBnXkFtZTcwNTA5OTY0OQ@@._V1_FMjpg_UX1000_.jpg'
     },
     {
+        id: 3,
         title: 'Money Heist',
         genre: 'Action, Heist, Mystery',
         description: 'An unusual group of robbers attempt to carry out the most perfect robbery in Spanish history - stealing 2.4 billion euros from the Royal Mint of Spain.',
