1ded5e00559907eeca73635ea301b608fdeffcdd
Nikolay <77922872+nBonev@users.noreply.github.com>
2025-01-21 15:20:51 +0200

    Add about page

--- src/index.js
+++ src/index.js
@@ -15,5 +15,9 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
 
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
