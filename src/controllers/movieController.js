df9fea3e65ef45d165b03572dcb1653d6b8d0815
Nikolay <77922872+nBonev@users.noreply.github.com>
2025-01-22 10:02:33 +0200

    Add create movie
    Add uuid

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -9,8 +9,9 @@ movieController.get('/create', (req, res) => {
 
 movieController.post('/create', (req, res) => {
     const newMovie = req.body;
+    movieService.create(newMovie);
 
-    res.end();
+    res.redirect('/');
 });
 
 movieController.get('/:movieId/details', (req, res) => {
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,3 +1,4 @@
+import { v4 as uuid } from 'uuid';
 import movies from "../movies.js";
 
 export default {
@@ -5,5 +6,18 @@ export default {
         const result = movies.find(movie => movie.id === movieId);
     
         return result;
+    },
+
+    create(movieData) {
+        const newId = uuid();
+        
+        movies.push({
+            id: newId,
+            ...movieData,
+        });
+
+        return newId;
+    },
+
     }
 }
