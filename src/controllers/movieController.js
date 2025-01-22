97a1d95162c37c887eca681e796ab3cced2f200f
Nikolay <77922872+nBonev@users.noreply.github.com>
2025-01-22 09:22:22 +0200

    Parse form data on server

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -7,6 +7,12 @@ movieController.get('/create', (req, res) => {
     res.render('create');
 });
 
+movieController.post('/create', (req, res) => {
+    const newMovie = req.body;
+
+    res.end();
+});
+
 movieController.get('/:movieId/details', (req, res) => {
     const movieId = req.params.movieId;
     const movie = movieService.findMovie(movieId);
