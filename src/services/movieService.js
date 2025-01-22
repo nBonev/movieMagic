5e02b9b935c1534a834cfd1c17b788b978aac77a
Nikolay <77922872+nBonev@users.noreply.github.com>
2025-01-22 11:05:15 +0200

    Add search by name

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -20,7 +20,13 @@ export default {
         return newId;
     },
 
-    getAll() {
-        return movies;
+    getAll(filter = {}) {
+        let result = movies;
+
+        if(filter.search){
+            result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()))
+        }
+
+        return result;
     }
 }
