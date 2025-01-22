5c2f361fe7bea79872d54e65a88222b0fcd985cd
Nikolay <77922872+nBonev@users.noreply.github.com>
2025-01-22 11:10:24 +0200

    Add search by genre and year

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -27,6 +27,14 @@ export default {
             result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()))
         }
 
+        if(filter.genre){
+            result = result.filter(movie => movie.genre.toLowerCase() === filter.genre);
+        }
+
+        if(filter.year){
+            result = result.filter(movie => movie.year === filter.year);
+        }
+
         return result;
     }
 }
