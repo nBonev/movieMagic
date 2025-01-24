c57d5c4fc6552f864c955e47b5299427c4b261ba
Nikolay <77922872+nBonev@users.noreply.github.com>
2025-01-24 10:08:57 +0200

    Refactor home page to use database movies

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,8 +3,15 @@ import movieService from '../services/movieService.js';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    const movies = movieService.getAll();
+router.get('/', async (req, res) => {
+    // *Second solution use .lean on query to get plain object
+    const movies = await movieService.getAll();
+
+    // *First solution - convert documents to objects
+    // Convert document to plain object
+    // const plainMovies = movies.map(m => m.toObject());
+
+    // *Third solution is to use allowProtoPropertiesByDefault runtimeOption in handlebars
     res.render('home', { movies });
 });
 
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,9 +1,10 @@
 import { v4 as uuid } from 'uuid';
-import movies from "../movies.js";
+import movies from '../movies.js';
+import Movie from '../models/Movie.js';
 
 export default {
     findMovie(movieId) {
-        const result = movies.find(movie => movie.id === movieId);
+        const result = Movie.findById(movieId);
     
         return result;
     },
@@ -21,19 +22,19 @@ export default {
     },
 
     getAll(filter = {}) {
-        let result = movies;
+        let result = Movie.find({});
 
-        if(filter.search){
-            result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()))
-        }
+        // if(filter.search){
+        //     result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()))
+        // }
 
-        if(filter.genre){
-            result = result.filter(movie => movie.genre.toLowerCase() === filter.genre);
-        }
+        // if(filter.genre){
+        //     result = result.filter(movie => movie.genre.toLowerCase() === filter.genre);
+        // }
 
-        if(filter.year){
-            result = result.filter(movie => movie.year === filter.year);
-        }
+        // if(filter.year){
+        //     result = result.filter(movie => movie.year === filter.year);
+        // }
 
         return result;
     }
