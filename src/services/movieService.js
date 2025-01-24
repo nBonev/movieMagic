53a8152d3a651c64ade6012fb15d620048bf0d1f
Nikolay <77922872+nBonev@users.noreply.github.com>
2025-01-24 13:19:57 +0200

    Attach cast to movie

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -33,5 +33,13 @@ export default {
         }
 
         return query;
+    },
+
+    async attachCast(movieId, castId) {
+        const movie = await Movie.findById(movieId);
+        movie.casts.push(castId);
+        await movie.save();
+
+        return movie;
     }
 }
