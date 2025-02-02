ce30bea5d390d45c3e494cd1e4ac43a3bcf8d040
Nikolay <77922872+nBonev@users.noreply.github.com>
2025-02-02 14:19:23 +0200

    Show details buttons if user is creator

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -26,8 +26,9 @@ movieController.post('/create', async (req, res) => {
 movieController.get('/:movieId/details', async (req, res) => {
     const movieId = req.params.movieId;
     const movie = await movieService.findMovieWithCasts(movieId);
+    const isCreator = movie.creator && movie.creator.toString() === req.user?.id;
 
-    res.render('movie/details', { movie });
+    res.render('movie/details', { movie, isCreator });
 });
 
 movieController.get('/:movieId/attach-cast', async (req, res) => {
