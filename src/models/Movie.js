75e0ec2226650581e1957457854f4ae3d9d8b541
Nikolay <77922872+nBonev@users.noreply.github.com>
2025-02-05 13:05:22 +0200

    Add movie model validation

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -2,14 +2,46 @@ import { Schema, model, Types } from 'mongoose';
 
 // Create schema
 const movieSchema = new Schema({
-    title: String,
+    title: {
+        type: String,
+        required: [true, 'Title is required!'],
+        minLength: [5, 'Title should be at least 5 characters long!'],
+        maxLength: 250,
+        match: [/^[a-zA-Z 0-9]+$/, 'Title should be alphanumeric, digits and whitespaces only!'],
+    },
     category: String,
-    genre: String,
-    director: String,
-    year: Number,
-    imageUrl: String,
-    rating: Number,
-    description: String,
+    genre: {
+        type: String,
+        required: [true, 'Genre is required!'],
+        minLength: [5, 'Genre should be at least 5 characters long!'],
+        maxLength: 250,
+        match: [/^[a-zA-Z 0-9]+$/, 'Genre should be alphanumeric, digits and whitespaces only!'],
+    },
+    director: {
+        type: String,
+        minLength: [5, 'Director should be at least 5 characters long!'],
+        maxLength: 250,
+        match: [/^[a-zA-Z 0-9]+$/, 'Director should be alphanumeric, digits and whitespaces only!'],
+    },
+    year: {
+        type: Number,
+        min: 1900,
+        max: 2025,
+    },
+    imageUrl: {
+        type: String,
+        match: /^https?:\/\//,
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+    },
+    description: {
+        type: String,
+        minLength: 20,
+        match: /^[a-zA-Z 0-9]+$/,
+    },
     casts: [{
         type: Types.ObjectId,
         ref: 'Cast',
