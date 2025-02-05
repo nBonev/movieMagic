3d35b5abd71b204deeaf44081597cb384c7fa2cf
Nikolay <77922872+nBonev@users.noreply.github.com>
2025-02-05 16:07:35 +0200

    Add error message util

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -9,7 +9,17 @@ const movieSchema = new Schema({
         maxLength: 250,
         match: [/^[a-zA-Z 0-9]+$/, 'Title should be alphanumeric, digits and whitespaces only!'],
     },
-    category: String,
+    category: {
+        type: String,
+        required: true,
+        enum: [
+            'tv-show',
+            'animation',
+            'movie',
+            'documentary',
+            'short-film'
+        ],
+    },
     genre: {
         type: String,
         required: [true, 'Genre is required!'],
@@ -34,6 +44,7 @@ const movieSchema = new Schema({
     },
     rating: {
         type: Number,
+        default: 1,
         min: 1,
         max: 5,
     },
--- src/models/User.js
+++ src/models/User.js
@@ -4,14 +4,17 @@ import bcrypt from "bcrypt";
 const userSchema = new Schema({
     email: {
         type: String,
+        required: true,
         unique: true,
+        lowercase: true,
         match: /\@[a-zA-Z]+.[a-zA-Z]+$/,
         minLength: 10,
     },
     password: {
         type: String,
         match: /^\w+$/,
-        minLength: 6,
+        minLength: [6, 'Password should be at least 6 characters!'],
+        trim: true,
     }, 
 });
 
