8bb354e6c45a4b64ef0332e24a4afc5860a9718a
Nikolay <77922872+nBonev@users.noreply.github.com>
2025-02-05 15:32:08 +0200

    Check repeat password

--- src/services/authService.js
+++ src/services/authService.js
@@ -5,7 +5,16 @@ import jwt from "jsonwebtoken";
 const SECRET = process.env.JWT_SECRET || 'BASICSECRET';
 
 export default {
-    register(userData) {
+    async register(userData) {
+        // if(userData.password !== userData.rePassword) {
+        //     throw new Error('Password mismatch!')
+        // }
+
+        const userCount = await User.countDocuments({email: userData.email});
+        if(userCount > 0) {
+            throw new Error('Email already exist!');
+        }
+
         return User.create(userData);
         
     },
